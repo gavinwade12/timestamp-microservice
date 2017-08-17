7d4db7efb20aa3de9be83587767051a1d9cbe553
System <none>
2017-08-17 14:14:29 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -7,13 +7,23 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 app.get("/:date", (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
   const time = {unix: null, natural: null};
-  let parsedDate = new Date(request.params.date);
+  
+  let dateToParse = req.params.date;
+  let testInt = parseInt(dateToParse);
+  if (testInt)
+    dateToParse = testInt;
+  let parsedDate = new Date(dateToParse);
   if (parsedDate) {
     time.unix = parsedDate.getTime();
+    let month = 
   }
-  res.end();
+  
+  res.send(JSON.stringify(time));
 });
 
 var listener = app.listen(process.env.PORT, () => console.log('listening on port: ' + process.env.PORT));
